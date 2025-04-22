e2d563ff722ecab072e0b6f92e012ca4ddeafbba
hum6le101 <johanneshandrock@hotmail.com>
2025-04-22 20:55:23 +0200

    My first project on boot.dev - update

--- stats.py
+++ stats.py
@@ -15,13 +15,15 @@ def char_count(path_to_file):
                 chars[char] = 1
         return chars
 
-def sort_on(char_count):
-        for char in char_count:
-            return char_count[char]
+def sort_on(char_dict):
+    # Extract the first (and only) key from the dictionary
+    char = list(char_dict.keys())[0]
+    # Return the value associated with that key
+    return char_dict[char]
 
 def sorting_by_char_count(char_count):
     sorted_chars = []
     for char in char_count:
-        sorted_chars.append({char : char_count[char]})
+        sorted_chars.append({char: char_count[char]})
     sorted_chars.sort(reverse=True, key=sort_on)
     return sorted_chars
